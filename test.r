9845ce078fa4e172db26e8499770a6d6ca2a26ac
kaif1808 <kaifaulkner1808@gmail.com>
2026-04-24 23:23:37 +0200

    fahhhh

---
+++ test.r
@@ -0,0 +1,12 @@
+load_pnadc(
+        save_to   = /Users/kai/Desktop/imHANKingit/new_function,
+        years     = 2015,
+        quarters  = 1:4,
+        panel     = 'advanced',
+        raw_data  = FALSE,
+        save_options = c(FALSE, FALSE),
+        output_vars = c('faixa_idade', 'sexo', 'faixa_educ',
+                        'rendimento_habitual_real', 'formal',
+                        'conta_propria', 'informal',
+                        'ocupado', 'desocupado', 'fora_forca_trab')
+      )
